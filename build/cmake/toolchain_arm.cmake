237db48cf9b3d2dc6f8b1f243a493262de86ae39
manju89jay <manjunath.jayaramaiah89@gmail.com>
2026-03-13 15:31:01 +0100

    commit toolchain cmake files

---
+++ build/cmake/toolchain_arm.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Os")
---
+++ build/cmake/toolchain_tricore.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR tricore)
+set(CMAKE_C_COMPILER tricore-elf-gcc)
+set(CMAKE_C_FLAGS "-mcpu=tc39xx -mfpu=single -Os -ffunction-sections -fdata-sections")
